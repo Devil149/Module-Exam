fb2a8a67f13883a753cb1bd85d920a4addf81204
Deepu Singh <singhdevil149@gmail.com>
2023-02-02 14:49:50 +0530

    NodeList inserted

--- NodeList.java
+++ NodeList.java
@@ -2,11 +2,12 @@ import java.util.LinkedList;
 
 class NodeList{
     public static void main(String[] args) {
-        
         LinkedList list = new LinkedList();
-        list.add("Deepu");
-        list.add("Singh");
-        list.add("Rohit");
-        list.add("Vijay"); 
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        
     }
 }
