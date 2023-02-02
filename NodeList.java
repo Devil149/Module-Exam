94f37b66bf55edd6d7be32a560888c04b931ee65
Deepu Singh <singhdevil149@gmail.com>
2023-02-02 14:58:09 +0530

    NodeList updated with insertionatend

--- NodeList.java
+++ NodeList.java
@@ -1,6 +1,32 @@
 import java.util.LinkedList;
 
 class NodeList{
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    public Node head = null;
+    public Node tail = null;
+
+    public void insertion_at_end(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        
+    }
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
