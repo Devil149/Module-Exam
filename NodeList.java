f97530ea98ba7660d5d18df651cd69722176248c
Deepu Singh <singhdevil149@gmail.com>
2023-02-02 15:16:38 +0530

    Dipslay method added

--- NodeList.java
+++ NodeList.java
@@ -1,6 +1,4 @@
-import java.util.LinkedList;
-
-class NodeList{
+public class NodeList{
     class Node{
         int data;
         Node next;
@@ -14,6 +12,18 @@ class NodeList{
     public Node head = null;
     public Node tail = null;
 
+    public void insertfirst(int data){
+        Node newNode = new Node(data);
+        Node temp = head;
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            head = newNode;
+            head.next = temp;
+        }
+    }
+
     public void insertion_at_end(int data){
         Node newNode = new Node(data);
 
@@ -22,18 +32,34 @@ class NodeList{
             tail = newNode;
         }
         else{
-            tail.next = newNode;
             tail = newNode;
+            tail.next = newNode;
         }
-        
+
+    }
+
+    public void display() {
+        Node current = head;
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        System.out.println("Adding a new node");
+        while(current!=null){
+            System.out.println(current.data +" ");
+            current = current.next;
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
 
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        
+        NodeList newNode = new NodeList();
+        newNode.insertfirst(10);
+        newNode.insertfirst(20);
+        newNode.insertfirst(30);
+        newNode.insertfirst(40);
+
+        newNode.insertion_at_end(50);
+        newNode.display();
     }
 }
